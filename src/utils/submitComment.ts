51365c1b1787aca1351cde0e416e75a62f62a1b7
Nicolas Barcellos <nicolasbarcellos95@gmail.com>
2022-03-24 09:28:11 -0300

    update env

--- src/utils/submitComment.ts
+++ src/utils/submitComment.ts
@@ -3,6 +3,9 @@ import { CommentData } from "../types";
 
 export const submitComment = async (obj: CommentData) => {
   const payload = obj;
-  const result = await axios.post(`/api/comments`, payload);
+  const result = await axios.post(
+    `${process.env.NEXT_PUBLIC_URL}/api/comments`,
+    payload
+  );
   return result;
 };
