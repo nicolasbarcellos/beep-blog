29b2b2579a11289f47c3c39f143c28983aa59820
Nicolas Barcellos <nicolasbarcellos95@gmail.com>
2022-03-24 09:46:26 -0300

    a1

--- src/utils/submitComment.ts
+++ src/utils/submitComment.ts
@@ -2,11 +2,8 @@ import axios from "axios";
 import { CommentData } from "../types";
 
 export const submitComment = async (obj: CommentData) => {
+  const url = "https://beep-blog.vercel.app/api/comments";
   const payload = obj;
-  const result = await axios.post(
-    `https://beep-blog.vercel.app/api/comments`,
-    payload
-  );
+  const result = await axios.post(url, payload);
   return result;
 };
-
